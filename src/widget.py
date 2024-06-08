657d5347b5f4c61ba1f0d9aa5d352a7ad6e439f6
Valentina Ovsianik <forovp@gmail.com>
2024-06-08 20:30:03 +0300

    добавлена функция маскировки счета

--- src/widget.py
+++ src/widget.py
@@ -7,3 +7,9 @@ def mask_account_card(info: str) - > str:
         if len (card_number) != 16:
             raise ValueError("Номер карты  должен состоять из 16 цифр")
          return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+    def get_mask_account(account_number: str) -> str:
+        """Функция маскирует номер счета в формате **XXXX"""
+        if len(account_number) < 4:
+            raise ValueError("Номер счета должен состоять не менее чем из 4 цифр")
+        return f"**{account_number[-4:]}"
