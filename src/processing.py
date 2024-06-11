c1760a9c1aef3698861487a5fa22bb8ad8960d0c
Valentina Ovsianik <forovp@gmail.com>
2024-06-12 00:02:27 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,10 +8,22 @@ def filter_by_state(data: list, state="EXECUTED": str) -> list:
 
     return  filtered_data
 
-# Примеры
+
 # data = {"id": 41428829, "state": "EXECUTED", "date": "2019"-07-03T18:35:29.512364"}
 #        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}
 
 # Выход функции
 # print(filter_by_state(data))
 # print(filter_by_state(data, "CANCELED"))
+
+
+def sort_by_date(date: list, reverse=True: bool) -> list:
+    """Функция  сортирует список словарей по дате"""
+    return sorted(data, key=lambda x: x["date"], reverse=reverse) # Сортировка выполняется по ключу "date". Часть "reverse=reverse" устанавливает порядок сортировки - убывание/возрастание
+
+sorted_data = sort_by_data(data)
+# print(sorted_data)
+
+# date = [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}, {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}, {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}, {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+
+
