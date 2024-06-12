6c5bb42e8470ca17d114cbee09111e39a269e0cf
Valentina Ovsianik <forovp@gmail.com>
2024-06-12 22:29:33 +0300

    changes made to processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,27 +6,24 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
         if item.get("state") == state:
             filtered_data.append(item)
 
-    return  filtered_data
-
-
-# data = [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]
-#        [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}]
-
-# Выход функции
-# print(filter_by_state(data))
-# print(filter_by_state(data, "CANCELED"))
+    return filtered_data
 
 
 def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
-    """Функция  сортирует список словарей по дате"""
-    return sorted(data, key=lambda x: x["date"], reverse=reverse) # Сортировка выполняется по ключу "date". Часть "reverse=reverse" устанавливает порядок сортировки - убывание (True)/возрастание (False)
+    """Функция сортирует список словарей по дате"""
+    sorted_data = sorted(data, key=lambda x: x["date"], reverse=reverse)  # Устанавливаем порядок сортировки
+    return sorted_data
 
-# Сортировка по убыванию, т. е. сначала последние операции
-sorted_data = sort_by_date(data)
-print(sorted_data)
+
+# data = [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}]
+
+# Выход функции фильтрации
+# print(filter_by_state(data))
+# print(filter_by_state(data, "CANCELED"))
+
+# Сортировка по убыванию
+# print(sort_by_date(data))
 
 # Сортировка по возрастанию
-sorted_data_asc = sort_by_date(data, reverse=False)
-print(sorted_data_asc)
-
-# date = [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}, {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}, {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}, {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+# print(sort_by_date(data, reverse=False))
