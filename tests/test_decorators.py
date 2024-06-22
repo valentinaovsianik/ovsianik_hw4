a96f3353cf6014db91ec19549ce93aa46ec81f94
Valentina Ovsianik <forovp@gmail.com>
2024-06-22 20:02:29 +0300

    Написаны и реализованы тесты для декоратора

---
+++ tests/test_decorators.py
@@ -0,0 +1,36 @@
+import pytest
+from src.decorators import log
+
+def test_log_file():
+    filename = "mylog.txt"
+
+    @log(filename=filename)
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(1, 2)
+
+    assert result == 3
+
+    with open(filename, "r") as f:
+        content = f.read().strip()
+
+    assert "my_function ok" in content
+
+
+def test_log_error():
+    filename = "mylog.txt"
+
+    @log(filename=filename)
+    def my_function(x, y):
+        return x / y
+
+    with pytest.raises(ZeroDivisionError):
+        my_function(1, 0)
+
+    error_message = "my_function error: ZeroDivisionError. Inputs: (1, 0), {}"
+
+    with open(filename, "r") as f:
+        content = f.read().strip()
+
+    assert error_message in content
