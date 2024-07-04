333f749f506a9278c8e4bd0ea14fdb6156a44242
Valentina Ovsianik <forovp@gmail.com>
2024-07-04 23:10:17 +0300

    Добавлены тесты для convert_currency

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import json
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from src.external_api import convert_currency
 
 
@@ -35,5 +35,35 @@ def test_convert_currency_error(mock_requests_get):
     transaction = {"amount": 100, "currency": "USD"}
 
     mock_requests_get.side_effect = requests.exceptions.RequestException("Connection error")
+    converted_amount = convert_currency(transaction)
+    assert converted_amount is None
+
+
+def test_convert_currency_missing_result_key(mock_requests_get):
+    """Тест для обработки отсутствия ключа 'result' в json-ответе"""
+    mock_requests_get.return_value = Mock(status_code=200)
+    mock_requests_get.return_value.json.return_value = {"info": "Some info", "query": "Some query"}
+
+    transaction = {"amount": 100, "currency": "USD"}
+    converted_amount = convert_currency(transaction)
+    assert converted_amount is None
+
+
+def test_convert_currency_key_error(mock_requests_get):
+    """Тест для обработки KeyError"""
+    mock_requests_get.return_value = Mock(status_code=200)
+    mock_requests_get.return_value.json.side_effect = KeyError("Missing key")
+
+    transaction = {"amount": 100, "currency": "USD"}
+    converted_amount = convert_currency(transaction)
+    assert converted_amount is None
+
+
+def test_convert_currency_value_error(mock_requests_get):
+    """Тест для обработки ValueError"""
+    mock_requests_get.return_value = Mock(status_code=200)
+    mock_requests_get.return_value.json.side_effect = ValueError("Invalid value")
+
+    transaction = {"amount": 100, "currency": "USD"}
     converted_amount = convert_currency(transaction)
     assert converted_amount is None
